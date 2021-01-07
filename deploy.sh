06946c333dae81d45436d3a07cfa3040515a805f
ngoc99vh <ngoc99vh@example.com>
2021-01-07 16:51:34 +0700

    deploy: fc26b58c4e31b76b21fbf79025f32f3a0a3e89b8

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,23 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-
 me=$(basename "$0")
 
 help_message="\
-Usage: $me [<options>] <command> [<command-options>]
-Run commands related to the slate process.
+Usage: $me [-c FILE] [<options>]
+Deploy generated files to a git branch.
 
-Commands:
+Options:
 
-  serve                   Run the middleman server process, useful for
-                          development.
-  build                   Run the build process.
-  deploy                  Will build and deploy files to branch. Use
-                          --no-build to only deploy.
-
-Global Options:
-
-  -h, --help              Show this help information.
-  -v, --verbose           Increase verbosity. Useful for debugging.
-
-Deploy options:
-  -e, --allow-empty       Allow deployment of an empty directory.
-  -m, --message MESSAGE   Specify the message used when committing on the
-                          deploy branch.
-  -n, --no-hash           Don't append the source commit's hash to the deploy
-                          commit's message.
-  --no-build              Do not build the source files.
+  -h, --help               Show this help information.
+  -v, --verbose            Increase verbosity. Useful for debugging.
+  -e, --allow-empty        Allow deployment of an empty directory.
+  -m, --message MESSAGE    Specify the message used when committing on the
+                           deploy branch.
+  -n, --no-hash            Don't append the source commit's hash to the deploy
+                           commit's message.
 "
 
-
-run_serve() {
-  exec bundle exec middleman serve --watcher-force-polling
-}
-
-run_build() {
-  bundle exec middleman build --clean
-}
+bundle exec middleman build --clean
 
 parse_args() {
   # Set args from a local environment file.
@@ -44,15 +25,13 @@ parse_args() {
     source .env
   fi
 
-  command=
-
   # Parse arg flags
   # If something is exposed as an environment variable, set/overwrite it
   # here. Otherwise, set/overwrite the internal variable instead.
   while : ; do
     if [[ $1 = "-h" || $1 = "--help" ]]; then
       echo "$help_message"
-      exit 0
+      return 0
     elif [[ $1 = "-v" || $1 = "--verbose" ]]; then
       verbose=true
       shift
@@ -65,25 +44,10 @@ parse_args() {
     elif [[ $1 = "-n" || $1 = "--no-hash" ]]; then
       GIT_DEPLOY_APPEND_HASH=false
       shift
-    elif [[ $1 = "--no-build" ]]; then
-      no_build=true
-      shift
-    elif [[ $1 = "serve" || $1 = "build" || $1 = "deploy" ]]; then
-      if [ ! -z "${command}" ]; then
-        >&2 echo "You can only specify one command."
-        exit 1
-      fi
-      command=$1
-      shift
-    elif [ -z $1 ]; then
+    else
       break
     fi
   done
-
-  if [ -z "${command}" ]; then
-    >&2 echo "Command not specified."
-    exit 1
-  fi
 
   # Set internal option vars from the environment and arg flags. All internal
   # vars should be declared here, with sane defaults if applicable.
@@ -104,6 +68,8 @@ parse_args() {
 }
 
 main() {
+  parse_args "$@"
+
   enable_expanded_output
 
   if ! git diff --exit-code --quiet --cached; then
@@ -131,7 +97,7 @@ main() {
     return 1
   fi
 
-  # must use short form of flag in ls for compatibility with macOS and BSD
+  # must use short form of flag in ls for compatibility with OS X and BSD
   if [[ -z `ls -A "$deploy_directory" 2> /dev/null` && -z $allow_empty ]]; then
     echo "Deploy directory '$deploy_directory' is empty. Aborting. If you're sure you want to deploy an empty tree, use the --allow-empty / -e flag." >&2
     return 1
@@ -174,7 +140,7 @@ incremental_deploy() {
     0) echo No changes to files in $deploy_directory. Skipping commit.;;
     1) commit+push;;
     *)
-      echo git diff exited with code $diff. Aborting. Staying on branch $deploy_branch so you can debug. To switch back to main, use: git symbolic-ref HEAD refs/heads/main && git reset --mixed >&2
+      echo git diff exited with code $diff. Aborting. Staying on branch $deploy_branch so you can debug. To switch back to master, use: git symbolic-ref HEAD refs/heads/master && git reset --mixed >&2
       return $diff
       ;;
   esac
@@ -234,15 +200,4 @@ sanitize() {
   "$@" 2> >(filter 1>&2) | filter
 }
 
-parse_args "$@"
-
-if [ "${command}" = "serve" ]; then
-  run_serve
-elif [[ "${command}" = "build" ]]; then
-  run_build
-elif [[ ${command} = "deploy" ]]; then
-  if [[ ${no_build} != true ]]; then
-    run_build
-  fi
-  main "$@"
-fi
+[[ $1 = --source-only ]] || main "$@"
